9eca17808175da4dad250c59ab7c36b2b362158d
USAxioms <michael@usaxioms.com>
2026-07-22 02:22:09 -0700

    Create r3-axioms.js

---
+++ math-r3/r3-axioms.js
@@ -0,0 +1,6 @@
+// Core Constitutional Axioms (Determinism, Supremacy, Transparency)
+export const AXIOMS = {
+  determinism: true,
+  zeroDependencyBackend: true,
+  wadArithmeticRequired: true,
+};
